05e8903d39fec875eacfe66b53e4e0c8014b56b7
leflores <pflores.fisi22@gmail.com>
2023-02-06 12:40:54 -0500

    remove unused module

--- src/utils.ts
+++
@@ -1,2 +0,0 @@
-const isRejected = (input: PromiseSettledResult<unknown>): input is PromiseRejectedResult => input.status === 'rejected';
-const isFulfilled = <T,>(input: PromiseSettledResult<T>): input is PromiseFulfilledResult<T> => input.status === 'fulfilled';
