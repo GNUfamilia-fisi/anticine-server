5a795fbc830b70bddc36ed550f1c3229e3e294d1
leflores <pflores.fisi22@gmail.com>
2023-02-07 10:48:06 -0500

    get port from env variables

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import { ipLookupLocation } from './geolocation.js';
 const app = express();
 app.set('trust proxy', true);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
